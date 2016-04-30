0d6f64ae50b7d9feacf3e0254c50394175c7b56c
jeremy-daily <jeremy@synercontechnologies.com>
2016-04-29 23:06:48 -0500

    Working CAN logger
    Needs J1708 Implementation

--- BasicCANLogger/BasicCANLogger.ino
+++ BasicCANLogger/BasicCANLogger.ino
@@ -11,28 +11,31 @@ Description: A Basic CAN logger
 
 #include <SPI.h>
 #include "SdFat.h"
-#include "FreeStack.h"
+//#include "FreeStack.h"
 
 #include <FlexCAN.h>
 
 // SD chip select pin.  Be sure to disable any other SPI devices such as Enet.
-const uint8_t SDchipSelect = A1;
+const uint8_t SDchipSelect = 10;
+const uint8_t CANchipSelect = A1;
 
 // Log file base name.  Must be six characters or less.
 #define CANFILE_BASE_NAME "CANlog"
+#define J1708FILE_BASE_NAME "J1708log"
 
 // global for card size
 uint32_t cardSize;
 
-const int CANRate = 500000;
+const int CANRate = 250000;
 
-FlexCAN CANbus(500000);
+FlexCAN CANbus(CANRate);
 
 static CAN_message_t txCANmsg,rxCANmsg;
 
 uint32_t txCANcount,rxCANcount;
 uint32_t txCANtimer,rxCANtimer;
 uint32_t currentMillis,previousMillis;
+boolean recording = true;
 
 // File system object.
 SdFat sd;
@@ -40,7 +43,6 @@ SdFat sd;
 // Log file.
 SdFile CANfile;
 
-char fileName[13];
 
 // Time in micros for next data record.
 uint32_t logTime;
@@ -50,8 +52,9 @@ void writeHeader() {
   CANfile.println(F("CAN Data Log"));
   CANfile.print(F("Rate,"));
   CANfile.println(CANRate, DEC);
-  CANfile.print(F("Line,"));
+  CANfile.print(F("Network,"));
   CANfile.print(F("Micros,"));
+  CANfile.print(F("Line,"));
   CANfile.print(F("ID,"));
   CANfile.print(F("DLC,"));
   CANfile.print(F("B0,"));
@@ -65,10 +68,12 @@ void writeHeader() {
   CANfile.println();
 }
 void logCANframe(){
-  CANfile.open(fileName);
-  CANfile.print(rxCANcount);
+  uint32_t currentMicros = micros();
+  CANfile.print("CAN");
   CANfile.print(",");
-  CANfile.print(rxCANtimer,DEC);
+  CANfile.print(currentMicros,DEC);
+  CANfile.print(",");
+  CANfile.print(rxCANcount);
   CANfile.print(",");
   CANfile.print(rxCANmsg.id,HEX);
   CANfile.print(",");
@@ -76,9 +81,21 @@ void logCANframe(){
   CANfile.print(",");
   for( int idx=0; idx<rxCANmsg.len; ++idx ) {
       CANfile.print(rxCANmsg.buf[idx],HEX);
+      CANfile.print(",");
+  
     }
   CANfile.println();
-  CANfile.close();
+  rxCANcount++;
+}
+void logJ1708byte(){
+  uint32_t currentMicros = micros();
+  uint8_t c = Serial3.read();
+  CANfile.print("J1708");
+  CANfile.print(",");
+  CANfile.print(currentMicros,DEC);
+  CANfile.print(",");
+  CANfile.println(c,HEX);
+  
 }
 
 void setup()
@@ -86,14 +103,17 @@ void setup()
   
   pinMode(SDchipSelect, OUTPUT);
   digitalWrite(SDchipSelect, HIGH);   // de-select the SD Card
- 
+  pinMode(CANchipSelect, OUTPUT);
+  digitalWrite(CANchipSelect, HIGH);   // de-select the SD Card
+  pinMode(13,OUTPUT);
+  digitalWrite(13,HIGH); //turn on LED
+  pinMode(0,INPUT_PULLUP);
+  
   Serial.begin(115200);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
+  delay(1000);
   
   Serial.println("Starting CAN Logger");
-  while (!sd.begin(SDchipSelect, SPI_HALF_SPEED)) {
+  while (!sd.begin(SDchipSelect, SPI_FULL_SPEED)) {
     delay(100);
     Serial.println("Error with SD Card.");
   }
@@ -139,14 +159,23 @@ void setup()
  
 
  
+ 
   
+  Serial.println("Starting CAN Bus");
+  CANbus.begin();
+  Serial3.begin(9600);
+  
+}
+
+
+void logData(){
   const uint8_t BASE_NAME_SIZE = sizeof(CANFILE_BASE_NAME) - 1;
+  
+  char fileName[13] = CANFILE_BASE_NAME "00.csv";
+   // Find an unused file name.
   if (BASE_NAME_SIZE > 6) {
     Serial.println("FILE_BASE_NAME too long");
   }
-
-  char fileName[13] = CANFILE_BASE_NAME "00.csv";
-
   while (sd.exists(fileName)) {
     if (fileName[BASE_NAME_SIZE + 1] != '9') {
       fileName[BASE_NAME_SIZE + 1]++;
@@ -159,27 +188,44 @@ void setup()
     }
   }
   
-  Serial.println("Starting CAN Bus");
-  CANbus.begin();
+  recording = digitalRead(0);
+  if (recording){
+      delay(150);
+      recording = digitalRead(0);    
+      if (recording){
+          if (!CANfile.open(fileName, O_CREAT | O_WRITE | O_APPEND)) {
+              Serial.println("file.open error!");
+              return;
+          }
+          Serial.print(F("Logging data to: "));
+          Serial.println(fileName);
+          writeHeader();
+          
+          while (recording) {
+            recording = digitalRead(0);
+            if (Serial3.available()) logJ1708byte();
+            while (CANbus.read(rxCANmsg)) logCANframe();
   
-  CANfile.open(fileName);
-  Serial.print(F("Logging CAN data to: "));
-  Serial.println(fileName);
-  writeHeader();
-  CANfile.close();
+            currentMillis = millis();
+            if (currentMillis - previousMillis >= 1000){
+              previousMillis = currentMillis;
+              Serial.println(rxCANcount);
+            }
+          }
+          
+          CANfile.close();
+          delay(1000);
+          digitalWrite(13,HIGH);
+          delay(1000);
+          digitalWrite(13,LOW);
+          rxCANcount = 0;
+        }
+     }
+    
 }
 
 void loop(){
-  currentMillis = millis();
+  logData();
   
-  while ( CANbus.read(rxCANmsg) ) {
-      rxCANtimer=micros();
-      rxCANcount++;
-      logCANframe();
-  }
-  if (currentMillis - previousMillis >= 1000){
-    previousMillis = currentMillis;
-    Serial.println(rxCANcount);
-  }
 }
 
